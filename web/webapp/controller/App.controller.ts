a4162f2ffc17727a40f937b9bfc9e56998d022cd
Nashitah Zainab <nashitahzainab@Nashitahs-MacBook-Air.local>
2024-08-29 10:37:52 +0200

    boot strap sap i5

--- web/webapp/controller/App.controller.ts
+++ web/webapp/controller/App.controller.ts
@@ -1,7 +1,26 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
+
+/**
+ * @name ui5.walkthrough.controller.App
+ */
 export default class AppController extends Controller {
-    public onInit(): void {
-        // Initialization logic
+	onInit(): void {
+      // set data model on view
+        const data = {
+           recipient: {
+              fname: "World2",
+              lname: "Mars"
+           }
+        };
+        const dataModel = new JSONModel(data);
+        this.getView()?.setModel(dataModel);
     }
-}
+
+    onShowHello(): void {
+        // show a native JavaScript alert
+        MessageToast.show("Hello World");
+     }
+};
