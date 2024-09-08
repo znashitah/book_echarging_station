b6634c9d8b635fccc6f7e9812b767ffb333e91a8
Nashitah Zainab <nashitahzainab@nashitahs-air.speedport.ip>
2024-09-08 08:19:12 +0200

    Display chargingstations list corresponding to user

--- web/webapp/controller/ChargingStation.controller.ts
+++ web/webapp/controller/ChargingStation.controller.ts
@@ -3,6 +3,10 @@ import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default class ChargingStationController extends Controller {
     public onInit(): void {
-        
+        const chargingstationModel = new JSONModel();
+        chargingstationModel.loadData("echargerbackend/chargingstations/db/1");
+        console.log(chargingstationModel);
+        this.getView()?.setModel(chargingstationModel, "chargingstationModel");
+
     }
 }
